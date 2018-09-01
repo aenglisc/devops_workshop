4efe1fb758fa48496b78540a01b940fa40ecb28c
Roman Pushkov <pushkovroman@me.com>
2018-09-01 14:26:21 +0300

    setup fix

--- make-app.mk
+++ make-app.mk
@@ -4,20 +4,21 @@ app:
 	docker-compose up
 
 app-bash:
+	docker-compose run --user=$(USER) app bash
+
+app-bash-root:
 	docker-compose run app bash
 
 app-build:
 	docker-compose build
 
-app-setup: app-build
-	docker-compose run app bin/setup
+app-setup: app-build app-install app-db-prepare
 
 app-install:
-	docker-compose run app mix deps.get
+	docker-compose run app make install
 
 app-db-drop:
 	docker-compose run app mix ecto.drop
 
 app-db-prepare:
-	docker-compose run app mix ecto.create
-	docker-compose run app mix ecto.migrate
+	docker-compose run app make db-prepare
